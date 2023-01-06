0d695833d36948d727c938dcbfcd799091f286e6
Selahattin <106350515+forsshilder@users.noreply.github.com>
2023-01-06 16:03:30 +0300

    Update index.js

--- Drum Kit Files/index.js
+++ Drum Kit Files/index.js
@@ -29,46 +29,46 @@ document.addEventListener("keypress",function(event){
 function makeSound(key){
   switch (key) {
     case "w":
-      var tom1 = new Audio("C:\\Users\\WARMACHINE\\Documents\\Web Development\\Drum Kit Starting Files\\sounds\\tom-1.mp3");
+      var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
 
     case "a":
-      var tom2 = new Audio("C:\\Users\\WARMACHINE\\Documents\\Web Development\\Drum Kit Starting Files\\sounds\\tom-2.mp3");
+      var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
 
     case "s":
-      var tom3 = new Audio("C:\\Users\\WARMACHINE\\Documents\\Web Development\\Drum Kit Starting Files\\sounds\\tom-3.mp3");
+      var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
 
 
 
     case "d":
-      var tom4 = new Audio("C:\\Users\\WARMACHINE\\Documents\\Web Development\\Drum Kit Starting Files\\sounds\\tom-4.mp3");
+      var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
 
     case "j":
-      var snare = new Audio("C:\\Users\\WARMACHINE\\Documents\\Web Development\\Drum Kit Starting Files\\sounds\\snare.mp3");
+      var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
 
 
     case "k":
-      var crash = new Audio("C:\\Users\\WARMACHINE\\Documents\\Web Development\\Drum Kit Starting Files\\sounds\\kick-bass.mp3");
+      var crash = new Audio("sounds/kick-bass.mp3");
       crash.play();
       break;
 
 
 
     case "l":
-      var kick = new Audio("C:\\Users\\WARMACHINE\\Documents\\Web Development\\Drum Kit Starting Files\\sounds\\crash.mp3");
+      var kick = new Audio("sounds/crash.mp3");
       kick.play();
       break;
 
